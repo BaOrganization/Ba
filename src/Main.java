15321b0709f670afb6b8b7121cfa75a4c198dba3
JulianWindeck <nope@nope.de>
2017-02-16 19:32:58 +0100

    Ok

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,6 @@ import java.awt.Dimension;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
 import javax.swing.SwingUtilities;
 
 /** Copyright: Ba
